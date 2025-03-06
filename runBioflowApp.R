31f56952df45bd7bb718798c32736e3431ac5d9a
Angela Pacheco <143752447+RAngelaPG@users.noreply.github.com>
2025-03-06 10:03:54 -0600

    Update runBioflowApp.R

--- runBioflowApp.R
+++ runBioflowApp.R
@@ -1,20 +1,20 @@
 .libPaths("./R-Portable/R-4.4.1/library")
 message('library paths:\n', paste('... ', .libPaths(), sep='', collapse='\n'))
 
-library(devtools)
-update_github <- function() {
-  pkgs = c("cgiarBase","cgiarPipeline","cgiarOcs","bioflow")
-  print(pkgs)
-  desc <- lapply(pkgs, packageDescription, lib.loc = NULL)
-  for (d in desc) {
-    message("working on ", d$Package)
-    if (!is.null(d$GithubSHA1)) {
-      message("Github found")
-      checkInt=curl::has_internet()
-	   if(checkInt==TRUE){install_github(repo = paste0(d$GithubUsername, "/", d$GithubRepo))}
-      install_github(repo = paste0(d$GithubUsername, "/", d$GithubRepo))
-    }
-  }
-}
-update_github()
+#library(devtools)
+#update_github <- function() {
+ # pkgs = c("cgiarBase","cgiarPipeline","cgiarOcs","bioflow")
+ # print(pkgs)
+ # desc <- lapply(pkgs, packageDescription, lib.loc = NULL)
+ # for (d in desc) {
+ #   message("working on ", d$Package)
+ #   if (!is.null(d$GithubSHA1)) {
+ #     message("Github found")
+ #     checkInt=curl::has_internet()
+ #	   if(checkInt==TRUE){install_github(repo = paste0(d$GithubUsername, "/", d$GithubRepo))}
+ #     install_github(repo = paste0(d$GithubUsername, "/", d$GithubRepo))
+ #   }
+ # }
+#}
+#update_github()
 bioflow::run_app()
